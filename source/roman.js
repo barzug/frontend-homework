24c5077d650ed200b14e65265b1d536e81c99de7
Sergey Barsukov <barsukof@live.com>
2017-08-28 23:38:04 +0300

    правильно объявил константу

--- source/roman.js
+++ source/roman.js
@@ -1,47 +1,6 @@
 'use strict';
 
-const roman = str => {
-    if (isNumeric(str)) {
-        return toRomanNumeral(str);
-    }
-
-    if (isRoman(str)) {
-        return fromRomanNumeral(str)
-    }
-
-    return null
-};
-
-
-const toRomanNumeral = num => {
-    let result = '';
-
-    Object.keys(lookup).forEach(i => {
-        while (num >= lookup[i]) {
-            result += i;
-            num -= lookup[i];
-        }
-    });
-
-    return result;
-};
-
-const fromRomanNumeral = str => {
-    let result = 0;
-
-    str.trim().toUpperCase().split('').reduce((prev, current) => {
-        result += lookup[prev] < lookup[current] ? lookup[current] - 2 * lookup[prev] : lookup[current];
-        return current;
-    }, "M");
-
-    return result;
-};
-
-const isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
-
-const isRoman = str => /^(\s*)(M{0,3})(D?C{0,3}|C[DM])(L?X{0,3}|X[LC])(V?I{0,4}|I[VX])(\s*)$/i.test(str);
-
-const lookup = {
+const ROMAN_LOOKUP = {
     M: 1000,
     CM: 900,
     D: 500,
@@ -55,4 +14,51 @@ const lookup = {
     V: 5,
     IV: 4,
     I: 1
+};
+
+const roman = str => {
+
+    const toRomanNumeral = num => {
+        let result = '';
+
+        Object.keys(ROMAN_LOOKUP).forEach(i => {
+            while (num >= ROMAN_LOOKUP[i]) {
+                result += i;
+                num -= ROMAN_LOOKUP[i];
+            }
+        });
+
+        return result;
+    };
+
+    const fromRomanNumeral = str => {
+        let result = 0;
+
+        str.trim().toUpperCase().split('').reduce((prev, current) => {
+
+            if (ROMAN_LOOKUP[prev] < ROMAN_LOOKUP[current]) {
+                result -= 2 * ROMAN_LOOKUP[prev];
+            }
+            result += ROMAN_LOOKUP[current];
+
+            return current;
+        }, "M");
+
+        return result;
+    };
+
+    const isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
+
+    const isRoman = str => /^(\s*)(M{0,3})(D?C{0,3}|C[DM])(L?X{0,3}|X[LC])(V?I{0,4}|I[VX])(\s*)$/i.test(str);
+
+
+    if (isNumeric(str)) {
+        return toRomanNumeral(str);
+    }
+
+    if (isRoman(str)) {
+        return fromRomanNumeral(str)
+    }
+
+    return null
 };
